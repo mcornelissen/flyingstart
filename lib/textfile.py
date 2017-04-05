1f05aee8e86e016fff643e90334ddc414c7c56e8
Michael Cornelissen <michael.cornelissen@altran.com>
2017-04-05 10:49:48 +0200

    Added custom exception for missing conf file

--- lib/textfile.py
+++ lib/textfile.py
@@ -1,14 +1,23 @@
 """ Print the contect of etc/text.conf to terminal
 """
 
+class MyCustomException(Exception):
+    def __init__(self, errormessage):
+        Exception.__init__(self,errormessage)
+        self.errormessage = errormessage
+
+
+
 def printtext():
     filetoread = "etc/text.conf"
     print('-'*20)
-    with open(filetoread, 'rt') as infile:
-        for line in infile:
-            print(line.strip())
-    print('-'*20)
-
+    try:
+        with open(filetoread, 'rt') as infile:
+            for line in infile:
+                print(line.strip())
+        print('-'*20)
+    except Exception, e:
+        raise MyCustomException("Wrong file")
 
 
 
